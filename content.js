563d8b006d39628156065b38600cf021bb35b932
Nagato <transcendentalliouville@gmail.com>
2025-02-14 21:16:12 +0900

    正常に「んご」，もしくは「こいつはりんごろう」に置換できない可能性があったので修正

--- content.js
+++ content.js
@@ -4,11 +4,15 @@
  */
 function replaceNgo(node) {
     if (node.nodeType === Node.TEXT_NODE) {
-        if (Math.floor(Math.random() * 5) === 0) {
-            node.textContent = node.textContent.replace(/。/g, "んご。こいつはりんごろう。");
-        } else {
-            node.textContent = node.textContent.replace(/。/g, "んご。");
-        }
+        node.textContent = node.textContent.replace(/。/g, 
+            function () {
+                if (Math.floor(Math.random() * 5) === 0) {
+                    return "んご。こいつはりんごろう。";
+                } else {
+                    return "んご。";
+                }
+            }
+        );
     } else {
         node.childNodes.forEach(replaceNgo);
     }
