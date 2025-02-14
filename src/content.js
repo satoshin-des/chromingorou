7857ab1f5ed259b668b981609f1a73c9128f69ec
Nagato <transcendentalliouville@gmail.com>
2025-02-14 20:24:02 +0900

    不要なコメントアウトを削除

--- src/content.js
+++ src/content.js
@@ -1,12 +1,13 @@
+/**
+ * 
+ * 文中の「。」を「んご。」，たまに「んご。こいつはりんごろう。」に置換する関数
+ */
 function replaceNgo(node) {
-    n = Math.floor(Math.random() * 5)
     if (node.nodeType === Node.TEXT_NODE) {
-        if (n % 5 === 0) {
+        if (Math.floor(Math.random() * 5) === 0) {
             node.textContent = node.textContent.replace(/。/g, "んご。こいつはりんごろう。");
-            // node.textContent = node.textContent.replace(/\./g, " Ngo. This is Ringorou.");
         } else {
             node.textContent = node.textContent.replace(/。/g, "んご。");
-            // node.textContent = node.textContent.replace(/\./g, " Ngo.");
         }
     } else {
         node.childNodes.forEach(replaceNgo);
