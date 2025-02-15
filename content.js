d15fdbad3b7efacc6aa6a5aee04979cc815f7e40
Nagato <transcendentalliouville@gmail.com>
2025-02-15 11:45:24 +0900

    仕様を変更し「りんご」を「山形りんご」に置換するように変更

--- content.js
+++ content.js
@@ -4,7 +4,8 @@
  */
 function replaceNgo(node) {
     if (node.nodeType === Node.TEXT_NODE) {
-        node.textContent = node.textContent.replace(/。/g, 
+        // 。->んご。
+        node.textContent = node.textContent.replace(/。/g,
             function () {
                 if (Math.floor(Math.random() * 5) === 0) {
                     return "んご。こいつはりんごろう。";
@@ -13,11 +14,41 @@ function replaceNgo(node) {
                 }
             }
         );
+
+        // ．->んご．
+        node.textContent = node.textContent.replace(/．/g,
+            function () {
+                if (Math.floor(Math.random() * 5) === 0) {
+                    return "んご．こいつはりんごろう．";
+                } else {
+                    return "んご．";
+                }
+            }
+        );
     } else {
         node.childNodes.forEach(replaceNgo);
     }
 }
 
+/**
+ * 
+ * 「りんご」とかを「山形りんご」とかに置換する関数
+ */
+function ringoToYamagata(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+        node.textContent = node.textContent.replace(/りんご/g, "山形りんご");
+        node.textContent = node.textContent.replace(/リンゴ/g, "山形リンゴ");
+        node.textContent = node.textContent.replace(/林檎/g, "山形林檎");
+
+        node.textContent = node.textContent.replace(/apple/g, "Yamagata apple");
+        node.textContent = node.textContent.replace(/Apple/g, "Yamagata apple");
+        node.textContent = node.textContent.replace(/APPLE/g, "YAMAGATA APPLE");
+    } else {
+        node.childNodes.forEach(ringoToYamagata);
+    }
+}
+
 window.onload = function () {
-    replaceNgo(document.body)
+    ringoToYamagata(document.body);
+    replaceNgo(document.body);
 }
